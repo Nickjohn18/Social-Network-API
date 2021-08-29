cfd65270ec5f2c0657aafdd43f95567f6ef64155
Nickjohn18 <81334326+Nickjohn18@users.noreply.github.com>
2021-08-29 14:26:00 -0400

    models

---
+++ models/thought.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 200,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.lenght;
+});
+
+const Thought = model("Thought", thoughtSchema);
+module.exports = Thought;
